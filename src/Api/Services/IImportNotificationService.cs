3cf131de3a2bd92641042fe02e0ba03302e24ed2
Tim Peel <tim.peel@equalexperts.com>
2025-04-03 17:30:23 +0100

    Linting

--- src/Api/Services/IImportNotificationService.cs
+++ src/Api/Services/IImportNotificationService.cs
@@ -5,6 +5,13 @@ namespace Defra.TradeImportsDataApi.Api.Services;
 public interface IImportNotificationService
 {
     Task<ImportNotificationEntity?> GetImportNotification(string chedId, CancellationToken cancellationToken);
-    Task<ImportNotificationEntity> Insert(ImportNotificationEntity importNotificationEntity, CancellationToken cancellationToken);
-    Task<ImportNotificationEntity> Update(ImportNotificationEntity importNotificationEntity, string etag, CancellationToken cancellationToken);
+    Task<ImportNotificationEntity> Insert(
+        ImportNotificationEntity importNotificationEntity,
+        CancellationToken cancellationToken
+    );
+    Task<ImportNotificationEntity> Update(
+        ImportNotificationEntity importNotificationEntity,
+        string etag,
+        CancellationToken cancellationToken
+    );
 }
--- src/Api/Services/ImportNotificationService.cs
+++ src/Api/Services/ImportNotificationService.cs
@@ -5,24 +5,34 @@ namespace Defra.TradeImportsDataApi.Api.Services;
 
 public class ImportNotificationService(IDbContext dbContext) : IImportNotificationService
 {
-    public async Task<ImportNotificationEntity?> GetImportNotification(string chedId, CancellationToken cancellationToken)
+    public async Task<ImportNotificationEntity?> GetImportNotification(
+        string chedId,
+        CancellationToken cancellationToken
+    )
     {
         return await dbContext.Notifications.Find(chedId, cancellationToken);
     }
 
-    public async Task<ImportNotificationEntity> Insert(ImportNotificationEntity importNotificationEntity, CancellationToken cancellationToken)
+    public async Task<ImportNotificationEntity> Insert(
+        ImportNotificationEntity importNotificationEntity,
+        CancellationToken cancellationToken
+    )
     {
         await dbContext.Notifications.Insert(importNotificationEntity, cancellationToken);
         await dbContext.SaveChangesAsync(cancellationToken);
-        
+
         return importNotificationEntity;
     }
 
-    public async Task<ImportNotificationEntity> Update(ImportNotificationEntity importNotificationEntity, string etag, CancellationToken cancellationToken)
+    public async Task<ImportNotificationEntity> Update(
+        ImportNotificationEntity importNotificationEntity,
+        string etag,
+        CancellationToken cancellationToken
+    )
     {
         await dbContext.Notifications.Update(importNotificationEntity, etag, cancellationToken);
         await dbContext.SaveChangesAsync(cancellationToken);
-        
+
         return importNotificationEntity;
     }
 }
