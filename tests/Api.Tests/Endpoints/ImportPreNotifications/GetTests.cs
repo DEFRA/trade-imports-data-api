bc99081eaa1ddb95094a251bc83c24b8c499f6bc
Tim Peel <tim.peel@equalexperts.com>
2025-04-11 14:39:00 +0100

    Replicate example domain test for json serialisation of IPAFFS data

--- tests/Api.Tests/Endpoints/ImportPreNotifications/GetTests.cs
+++ tests/Api.Tests/Endpoints/ImportPreNotifications/GetTests.cs
@@ -1,6 +1,9 @@
 using System.Net;
+using System.Text.Json;
+using AutoFixture;
 using Defra.TradeImportsDataApi.Api.Services;
 using Defra.TradeImportsDataApi.Data.Entities;
+using Defra.TradeImportsDataApi.Domain.Gvms;
 using Defra.TradeImportsDataApi.Domain.Ipaffs;
 using Defra.TradeImportsDataApi.Testing;
 using FluentAssertions;
@@ -18,6 +21,7 @@ public class GetTests : EndpointTestBase, IClassFixture<WireMockContext>
     private WireMockServer WireMock { get; }
     private const string ChedId = "chedId";
     private readonly VerifySettings _settings;
+    private static readonly JsonSerializerOptions s_jsonOptions = new() { WriteIndented = true };
 
     public GetTests(ApiWebApplicationFactory factory, ITestOutputHelper outputHelper, WireMockContext context)
         : base(factory, outputHelper)
@@ -28,6 +32,7 @@ public class GetTests : EndpointTestBase, IClassFixture<WireMockContext>
         _settings = new VerifySettings();
         _settings.ScrubMember("traceId");
         _settings.DontScrubDateTimes();
+        _settings.DontScrubGuids();
     }
 
     protected override void ConfigureTestServices(IServiceCollection services)
@@ -90,5 +95,52 @@ public class GetTests : EndpointTestBase, IClassFixture<WireMockContext>
         var response = await client.GetAsync(TradeImportsDataApi.Testing.Endpoints.ImportPreNotifications.Get(ChedId));
 
         response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+
+    [Fact]
+    public async Task Get_WhenGenerating_GetTests_DomainExample_ShouldSerialize()
+    {
+        var fixture = new Fixture();
+        fixture.Customize<DateOnly>(o => o.FromFactory((DateTime dt) => DateOnly.FromDateTime(dt)));
+        fixture.Customize<TimeOnly>(o => o.FromFactory((DateTime dt) => TimeOnly.FromDateTime(dt)));
+        var importPreNotification = fixture.Create<ImportPreNotification>();
+        var serialized = JsonSerializer.Serialize(importPreNotification, s_jsonOptions);
+
+        // Take this file and replace GetTests_DomainExample.json when needed
+        await File.WriteAllTextAsync(
+            $"{nameof(Get_WhenGenerating_GetTests_DomainExample_ShouldSerialize)}_ImportPreNotification.json",
+            serialized
+        );
+
+        serialized.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task Get_WhenReturningDomainExample_ShouldBeCorrectJson()
+    {
+        // See test above for generation of new content
+        var body = EmbeddedResource.GetBody(GetType(), "GetTests_DomainExample.json");
+        var importPreNotification =
+            JsonSerializer.Deserialize<ImportPreNotification>(body, s_jsonOptions)
+            ?? throw new InvalidOperationException();
+        var client = CreateClient();
+        MockImportPreNotificationService
+            .GetImportPreNotification(ChedId, Arg.Any<CancellationToken>())
+            .Returns(
+                new ImportPreNotificationEntity
+                {
+                    Id = ChedId,
+                    ImportPreNotification = importPreNotification,
+                    Created = new DateTime(2025, 4, 3, 10, 0, 0, DateTimeKind.Utc),
+                    Updated = new DateTime(2025, 4, 3, 10, 15, 0, DateTimeKind.Utc),
+                    ETag = "etag",
+                }
+            );
+
+        var response = await client.GetStringAsync(
+            TradeImportsDataApi.Testing.Endpoints.ImportPreNotifications.Get(ChedId)
+        );
+
+        await VerifyJson(response, _settings).UseStrictJson();
     }
 }
