86840f37ccbeadb77ef8b52e7b2a677c0551ba9d
Tim Peel <tim.peel@equalexperts.com>
2025-04-23 12:29:32 +0100

    Add ResourceEventChildResourceTypes for consumers, which represents the current known types

---
+++ src/Domain/Events/ResourceEventChildResourceTypes.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Defra.TradeImportsDataApi.Domain.Events;
+
+[ExcludeFromCodeCoverage]
+// ReSharper disable once UnusedType.Global
+public static class ResourceEventChildResourceTypes
+{
+    public const string ClearanceRequest = nameof(ClearanceRequest);
+
+    public const string ClearanceDecision = nameof(ClearanceDecision);
+
+    public const string Finalisation = nameof(Finalisation);
+}
