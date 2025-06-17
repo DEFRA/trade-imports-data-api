2f9791ad97fd72af24eb6ff70f487109f28e5fb7
Tim Peel <tim.peel@equalexperts.com>
2025-06-17 10:09:14 +0100

    Ignore certain paths if they start with specific values

--- src/Api/Metrics/MetricsMiddleware.cs
+++ src/Api/Metrics/MetricsMiddleware.cs
@@ -5,10 +5,13 @@ namespace Defra.TradeImportsDataApi.Api.Metrics;
 [ExcludeFromCodeCoverage]
 public class MetricsMiddleware(RequestMetrics requestMetrics) : IMiddleware
 {
+    private static readonly string[] s_startsWith = ["/apple-", "/favicon", "/redoc", "/.well-known/openapi"];
+
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         var startingTimestamp = TimeProvider.System.GetTimestamp();
         var path = (context.GetEndpoint() as RouteEndpoint)?.RoutePattern.RawText ?? context.Request.Path;
+
         try
         {
             await next(context);
@@ -19,12 +22,17 @@ public class MetricsMiddleware(RequestMetrics requestMetrics) : IMiddleware
         }
         finally
         {
-            requestMetrics.RequestCompleted(
-                path,
-                context.Request.Method,
-                context.Response.StatusCode,
-                TimeProvider.System.GetElapsedTime(startingTimestamp).TotalMilliseconds
-            );
+            if (!IgnoreRequest(path))
+            {
+                requestMetrics.RequestCompleted(
+                    path,
+                    context.Request.Method,
+                    context.Response.StatusCode,
+                    TimeProvider.System.GetElapsedTime(startingTimestamp).TotalMilliseconds
+                );
+            }
         }
     }
+
+    private static bool IgnoreRequest(string path) => s_startsWith.Any(path.StartsWith);
 }
