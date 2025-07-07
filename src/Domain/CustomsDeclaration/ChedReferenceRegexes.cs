1852fe299232bb9f99eb5b33283219bdca3b9899
Thomas Anderson <t11omas1983@live.co.uk>
2025-07-07 08:56:26 +0100

    Changed the regex to look at the last 7 digits, but also include the last char is its V or R

--- src/Domain/CustomsDeclaration/ChedReferenceRegexes.cs
+++ src/Domain/CustomsDeclaration/ChedReferenceRegexes.cs
@@ -5,6 +5,6 @@ namespace Defra.TradeImportsDataApi.Domain.CustomsDeclaration;
 // <summary> Common Health Entry Document reference regular expressions. </summary>
 public static partial class ChedReferenceRegexes
 {
-    [GeneratedRegex("\\d{2,4}\\.?\\d{7}", RegexOptions.Compiled | RegexOptions.IgnoreCase)]
+    [GeneratedRegex("\\d{7}(v|r)?$", RegexOptions.Compiled | RegexOptions.IgnoreCase)]
     internal static partial Regex DocumentReferenceIdentifier();
 }
