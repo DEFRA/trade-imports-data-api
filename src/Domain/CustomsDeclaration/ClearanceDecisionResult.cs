6228d6023e157566adac918346ed46b2a4df3ea7
Tim Peel <tim.peel@equalexperts.com>
2025-07-11 15:26:13 +0100

    Add clearance decision results

---
+++ src/Domain/CustomsDeclaration/ClearanceDecisionResult.cs
@@ -0,0 +1,27 @@
+using System.Text.Json.Serialization;
+
+namespace Defra.TradeImportsDataApi.Domain.CustomsDeclaration;
+
+public class ClearanceDecisionResult
+{
+    [JsonPropertyName("itemNumber")]
+    public int ItemNumber { get; set; }
+
+    [JsonPropertyName("importPreNotification")]
+    public string? ImportPreNotification { get; set; }
+
+    [JsonPropertyName("documentReference")]
+    public string? DocumentReference { get; set; }
+
+    [JsonPropertyName("checkCode")]
+    public string? CheckCode { get; set; }
+
+    [JsonPropertyName("decisionCode")]
+    public string? DecisionCode { get; set; }
+
+    [JsonPropertyName("decisionReason")]
+    public string? DecisionReason { get; set; }
+
+    [JsonPropertyName("internalDecisionCode")]
+    public string? InternalDecisionCode { get; set; }
+}
