e929744b7e162e59c17f67bcc0448ec693775050
Thomas Andoerson <t11omas1983@live.co.uk>
2025-11-24 11:24:03 +0000

    exclude from code coverage

--- src/Api/Utils/JsonSettings.cs
+++ src/Api/Utils/JsonSettings.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 
 namespace Defra.TradeImportsDataApi.Api.Utils;
 
+[ExcludeFromCodeCoverage]
 public static class JsonSettings
 {
     public static readonly JsonSerializerOptions Instance = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
