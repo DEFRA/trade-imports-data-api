d97d2e9cb4cf2b7d6cc52bf2c351d915310fe8b6
Thomas Andoerson <t11omas1983@live.co.uk>
2026-01-19 15:52:04 +0000

    removed redundant code

--- src/Api/Services/IGmrService.cs
+++ src/Api/Services/IGmrService.cs
@@ -6,11 +6,6 @@ public interface IGmrService
 {
     Task<GmrEntity?> GetGmr(string id, CancellationToken cancellationToken);
 
-    Task<GmrEntity?> GetGmr(
-        System.Linq.Expressions.Expression<Func<GmrEntity, bool>> predicate,
-        CancellationToken cancellationToken
-    );
-
     Task<List<GmrEntity>> GetGmrByChedId(string chedId, CancellationToken cancellationToken);
 
     Task<GmrEntity> Insert(GmrEntity entity, CancellationToken cancellationToken);
